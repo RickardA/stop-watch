85dfb620a74eec29a4582042aa80b55d78f459d0
RickardA <rickard.andersson98@gmail.com>
2022-03-29 21:40:44 +0200

    calculate time

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -14,7 +15,13 @@ import (
 
 var white = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
 
+type StopWatch struct {
+	timer *time.Ticker
+}
+
 func main() {
+	sw := StopWatch{}
+
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello")
 
@@ -26,9 +33,23 @@ func main() {
 
 	myWindow.Resize(fyne.NewSize(800, 300))
 
-	topBarContainer := createTopBarContainer()
+	timerText, topBarContainer := createTopBarContainer()
 
-	bottomBarContainer := createBottomBarContainer()
+	bottomBarContainer := createBottomBarContainer(func() {
+		// Start
+		sw.timer = time.NewTicker(time.Millisecond)
+
+		go func(timerText *canvas.Text) {
+			counter := 0
+			for range sw.timer.C {
+				counter += 1
+				timerText.Text = msToTime(counter)
+				timerText.Refresh()
+			}
+		}(timerText)
+	}, func() {
+		sw.timer.Stop()
+	})
 
 	mainContainer := container.New(layout.NewBorderLayout(topBarContainer, bottomBarContainer, nil, nil),
 		topBarContainer, bottomBarContainer, content)
@@ -38,15 +59,38 @@ func main() {
 	myApp.Run()
 }
 
-func createTopBarContainer() *fyne.Container {
-	timerText := canvas.NewText("00:00:00", color.White)
-	timerText.TextSize = 50
-	return container.New(layout.NewHBoxLayout(), layout.NewSpacer(), timerText, layout.NewSpacer())
+func msToTime(ms int) string {
+	minutes := 0
+	seconds := 0
+	centiSeconds := 0
+
+	centiMs := ms % 1000
+	if centiMs != 0 {
+		centiSeconds = centiMs / 10
+	}
+
+	ms = ms - centiMs
+
+	tempSec := ms / 1000
+
+	seconds = tempSec % 60
+
+	tempSec = tempSec - seconds
+
+	minutes = tempSec / 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", minutes, seconds, centiSeconds)
 }
 
-func createBottomBarContainer() *fyne.Container {
-	startBtn := widget.NewButton("STARTA", func() {})
-	stopBtn := widget.NewButton("STANNA", func() {})
+func createTopBarContainer() (*canvas.Text, *fyne.Container) {
+	timerText := canvas.NewText("00:00:00", color.White)
+	timerText.TextSize = 50
+	return timerText, container.New(layout.NewHBoxLayout(), layout.NewSpacer(), timerText, layout.NewSpacer())
+}
+
+func createBottomBarContainer(startCallback func(), stopCallback func()) *fyne.Container {
+	startBtn := widget.NewButton("STARTA", startCallback)
+	stopBtn := widget.NewButton("STANNA", stopCallback)
 	btnContainer := container.New(layout.NewHBoxLayout(), startBtn, stopBtn)
 	bottomBarContainer := container.New(layout.NewCenterLayout(), layout.NewSpacer(), btnContainer, layout.NewSpacer())
 
